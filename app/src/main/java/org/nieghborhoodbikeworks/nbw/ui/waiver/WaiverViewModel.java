508917dea9c7d0abe42f48dfedf4338f3953b407
Rudy Hernandez <rhernande1@haverford.edu>
2018-11-13 14:42:25 -0500

    Started Waiver Fragment

---
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/waiver/WaiverViewModel.java
@@ -0,0 +1,7 @@
+package org.nieghborhoodbikeworks.nbw.ui.waiver;
+
+import android.arch.lifecycle.ViewModel;
+
+public class WaiverViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
