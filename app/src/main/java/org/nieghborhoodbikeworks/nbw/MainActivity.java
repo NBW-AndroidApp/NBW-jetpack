88531566f90d79ff4c395c115b79c13c34294cc0
Jackson <jmeyerlee@haverford.edu>
2018-11-13 13:45:53 -0500

    relaunch

---
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/MainActivity.java
@@ -0,0 +1,20 @@
+package org.nieghborhoodbikeworks.nbw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.nieghborhoodbikeworks.nbw.ui.login.LoginFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, LoginFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/login/LoginViewModel.java
@@ -0,0 +1,7 @@
+package org.nieghborhoodbikeworks.nbw.ui.login;
+
+import android.arch.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
