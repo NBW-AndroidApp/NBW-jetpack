ec3afb07a6ea6eeebcedf14e07c453b5f58898ca
Rudy Hernandez <rhernande1@haverford.edu>
2018-12-02 20:23:03 -0500

    Updated Waiver Fragment layout

--- app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/waiver/WaiverFragment.java
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/waiver/WaiverFragment.java
@@ -20,12 +20,11 @@ public class WaiverFragment extends Fragment {
 
     private WaiverViewModel mViewModel;
 
-    private Activity mContext;
-
     public static WaiverFragment newInstance() {
         return new WaiverFragment();
     }
 
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -38,29 +37,5 @@ public class WaiverFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(WaiverViewModel.class);
         // TODO: Use the ViewModel
     }
-
-    public void show() {
-        String message = mContext.getString(R.string.agreement);
-        mContext.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton(R.string.accept, new Dialog.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setNegativeButton(R.string.decline, new Dialog.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        mContext.finish();
-                    }
-                });
-        builder.create().show();
-    }
-
 
 }
