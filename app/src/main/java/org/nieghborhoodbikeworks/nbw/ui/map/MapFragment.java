b46c93244b2e06784f746aa05e2f34a6f9e44e3f
Jackson <jmeyerlee@haverford.edu>
2018-12-04 13:26:33 -0500

    got titles on the sidebar

---
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/map/MapFragment.java
@@ -0,0 +1,35 @@
+package org.nieghborhoodbikeworks.nbw.ui.map;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.nieghborhoodbikeworks.nbw.R;
+
+public class MapFragment extends Fragment {
+
+    private MapViewModel mViewModel;
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
