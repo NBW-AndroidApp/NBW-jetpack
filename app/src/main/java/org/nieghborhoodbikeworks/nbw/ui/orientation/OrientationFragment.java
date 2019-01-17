2c914640051e04e68049786ff144759b38831610
Rudy Hernandez <rhernande1@haverford.edu>
2019-01-16 19:52:11 -0500

    Cleaned UI for User Choice Fragment and Map Fragment

--- app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/orientation/OrientationFragment.java
+++ app/src/main/java/org/nieghborhoodbikeworks/nbw/ui/orientation/OrientationFragment.java
@@ -1,11 +1,13 @@
 package org.nieghborhoodbikeworks.nbw.ui.orientation;
 
-import android.app.Activity;
 import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +16,15 @@ import org.nieghborhoodbikeworks.nbw.DrawerLocker;
 import org.nieghborhoodbikeworks.nbw.R;
 import org.nieghborhoodbikeworks.nbw.SharedViewModel;
 
+import java.util.ArrayList;
+
 public class OrientationFragment extends Fragment {
 
     private View mView;
     private SharedViewModel mViewModel;
-    private Activity mContext;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<String> mVideos;
 
     public static OrientationFragment newInstance() {
         return new OrientationFragment();
@@ -29,6 +35,14 @@ public class OrientationFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         ((DrawerLocker)getActivity()).setDrawerLocked(false);
         mView = inflater.inflate(R.layout.orientation_fragment, container, false);
+        mRecyclerView = mView.findViewById(R.id.video_recycler_view);
+        mVideos = new ArrayList<>();
+        mVideos.add("Title");
+        mVideos.add("Video 1");
+        mAdapter = new OrientationAdapter(getActivity(), mVideos);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
         return mView;
     }
 
@@ -36,9 +50,6 @@ public class OrientationFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
-        // TODO: Use the ViewModel
-
-
     }
 
 }
